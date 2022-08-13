e7cef181647a79a37765470336e2366588caa499
Willian Ribeiro <mynamewra@gmail.com>
2022-08-13 12:44:19 -0300

    Template

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ python3 -m venv venv
 source venv/bin/activate  (caso não tenha sudo apt install python3.8-venv)
 pip install -r requirements.txt
 python -m flask --debug run
+
+
+## Atualizar pacotes 
--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+import os, json
+from flask import Flask, render_template, request, url_for, redirect
 
-app = Flask("projeto")
 
-@app.route("/")
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+@app.route("/",  methods=['GET'])
 def home():
-    return render_template('home.html', context={"msg":"Sucesso"})
+    with open(os.path.join(basedir, 'mockdata.json')) as f:
+        data = json.load(f)
+    return render_template('home.html', data=data)
+
+@app.route("/create-client")
+def create_client():
+    return render_template('form-client.html' )
+
+@app.route("/update-client/<int:id>'")
+def update_client(id):
+    return f'Update {id} !'
+
+@app.route("/remove-client/<int:id>'")
+def remove_client(id):
+    return f'Removido {id} !'
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
